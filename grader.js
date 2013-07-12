0dbd348331dc495ed319150a9962137036e6972f
Can Yagli <canyagli@gmail.com>
2013-07-12 03:30:00 +0000

    Modified grader.js to take URL as an argument, seems to be working, but will need to verify.

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,12 +65,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(url, data) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", url);
+            fs.writeFileSync(csvfile, result)
+        }
+    };
+    return response2console;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_adress>', 'The URL to show the application', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    // Check if a URL has been given as an argument
+    if(!program.url == URL_DEFAULT) 
+    {
+      console.log("\nURL is " + program.url);
+      var response2console = buildfn(url, data);
+      rest.get(apiurl).on('complete', response2console);
+      var htmlInput = rest.get(program.url).on('complete', response2console);
+    }
+    else {var htmlInput = program.file;}
+    var checkJson = checkHtmlFile(htmlInput, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {

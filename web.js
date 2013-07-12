970408d0751a125ce34f9d72ee7c91af0df4ce75
Can Yagli <canyagli@gmail.com>
2013-07-12 00:32:55 +0000

    Modified web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index_file = "index.html";
+  response.send(fs.readFileSync(index_file).toString());
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
